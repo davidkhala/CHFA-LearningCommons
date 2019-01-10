4084ca3baf6f757d07c36d2e55c63e72f68b3680
davidliu <david-khala@hotmail.com>
2019-01-10 23:02:53 +0800

    enhance prepare script

--- install.sh
+++ install.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env bash
 set -e
 CURRENT=$(cd $(dirname ${BASH_SOURCE}) && pwd)
+
+dockerVersion=18.06
+composeVersion=1.22.0
 function gitSync() {
 	git pull
-	git submodule update --init --recursive
+}
+function prepare(){
+    sudo apt -qq install -y curl
+    
+    # TODO install docker
+    docker version
+
+    if ! docker-compose version;then
+    sudo curl -L https://github.com/docker/compose/releases/download/${composeVersion}/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    fi
+    
+    sudo gpasswd -a $USER docker
+    newgrp docker # logout of current user is required before taking effect 
 }
 function fabricInstall(){
-    sudo apt install curl
-    curl -sSL http://bit.ly/2ysbOFE | bash -s 1.3.0
+    curl -sSL http://bit.ly/2ysbOFE | bash -s 1.3.0 1.3.0 0.4.13 # version should be specific, otherwize error in line:181
 }
 $1
